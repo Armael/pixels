d492fb44fbb7e4a9ed7796c88e1a2b99f32d349d
Armaël Guéneau <armael.gueneau@ens-lyon.org>
2024-01-21 20:50:06 +0100

    client/client{,2}.ml: enable passing ip/port as cmdline options

--- client/client2.ml
+++ client/client2.ml
@@ -7,11 +7,20 @@ let send_px buf sock x y r g b =
   ignore @@ Unix.send sock buf 0 (Bytes.length buf) []
 
 let () =
+  let ip = ref "127.0.0.1" in
+  let port = ref 4242 in
+  Arg.(parse [
+    "--ip", Set_string ip, Printf.sprintf "IP address (default: %s)" !ip;
+    "--port", Set_int port, Printf.sprintf "network port (default: %d)" !port;
+  ] (fun _ -> failwith "unexpected argument")
+    (Printf.sprintf "usage: %s [..options]" Sys.argv.(0))
+  );
+
   let x = ref 0 in
   let y = ref 0 in
 
   let sock = Unix.(socket PF_INET SOCK_STREAM 0) in
-  let addr = Unix.(ADDR_INET (inet_addr_of_string "127.0.0.1", 4242)) in
+  let addr = Unix.(ADDR_INET (inet_addr_of_string !ip, !port)) in
   Unix.connect sock addr;
   let buf = Bytes.create 7 in
   while true do
